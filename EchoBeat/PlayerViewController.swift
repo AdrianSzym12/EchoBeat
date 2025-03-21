4cd51bd6aa7515f2f5774b34d6848d7230cc9a87
Adrian Puchalski <8adbamdom@gmail.com>
2025-03-22 00:15:29 +0100

    feat: add splash screen on launch; add music player time;

--- EchoBeat/PlayerViewController.swift
+++ EchoBeat/PlayerViewController.swift
@@ -6,7 +6,7 @@ class PlayerViewController: UIViewController {
     public var position: Int = 0
     public var songs: [Song] = []
     @IBOutlet var holder: UIView!
-    var player: AVAudioPlayer!
+    var player: AVAudioPlayer?
     
     private let albumImageView: UIImageView = {
         let imageView = UIImageView()
@@ -35,9 +35,27 @@ class PlayerViewController: UIViewController {
         return label
     }()
     
-    private let playPauseButton = UIButton()
+    private let playPauseButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Play", for: .normal)
+        button.addTarget(self, action: #selector(playPauseTapped), for: .touchUpInside)
+        return button
+    }()
+    
     private var progressSlider: UISlider!
     private var timer: Timer?
+    
+    private let currentTimeLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let durationLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,7 +63,7 @@ class PlayerViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if holder.subviews.count == 0 {
+        if holder.subviews.isEmpty {
             configure()
         }
     }
@@ -58,10 +76,8 @@ class PlayerViewController: UIViewController {
             try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
             
             if let fileURL = song.fileURL {
-                // Odtwarzanie pliku dodanego przez użytkownika
                 player = try AVAudioPlayer(contentsOf: fileURL)
             } else if let trackName = song.trackName, let url = Bundle.main.url(forResource: trackName, withExtension: "mp3") {
-                // Odtwarzanie pliku wbudowanego w aplikację
                 player = try AVAudioPlayer(contentsOf: url)
             }
             
@@ -75,55 +91,37 @@ class PlayerViewController: UIViewController {
             print("Błąd odtwarzania pliku:", error)
         }
         
-        // album cover
         let imageSize = holder.frame.size.width - 40
         albumImageView.frame = CGRect(x: 20, y: 30, width: imageSize, height: imageSize)
         albumImageView.image = UIImage(named: song.imageName)
         holder.addSubview(albumImageView)
         
-        // music slider
         progressSlider = UISlider(frame: CGRect(x: 20, y: albumImageView.frame.maxY + 10, width: holder.frame.width - 40, height: 30))
         progressSlider.minimumValue = 0
         progressSlider.maximumValue = Float(player?.duration ?? 1)
         progressSlider.value = 0
         progressSlider.addTarget(self, action: #selector(progressSliderChanged(_:)), for: .valueChanged)
         holder.addSubview(progressSlider)
-
-        // song info
-        songNameLabel.frame = CGRect(x: 10, y: progressSlider.frame.maxY + 10, width: holder.frame.size.width - 20, height: 40)
+        
+        currentTimeLabel.frame = CGRect(x: 20, y: progressSlider.frame.maxY + 5, width: 50, height: 20)
+        durationLabel.frame = CGRect(x: holder.frame.width - 70, y: progressSlider.frame.maxY + 5, width: 50, height: 20)
+        holder.addSubview(currentTimeLabel)
+        holder.addSubview(durationLabel)
+        
+        songNameLabel.frame = CGRect(x: 10, y: durationLabel.frame.maxY + 10, width: holder.frame.size.width - 20, height: 40)
         songNameLabel.text = song.name
         holder.addSubview(songNameLabel)
-
+        
         albumNameLabel.frame = CGRect(x: 10, y: songNameLabel.frame.maxY + 5, width: holder.frame.size.width - 20, height: 30)
         albumNameLabel.text = song.albumName
         holder.addSubview(albumNameLabel)
-
+        
         artistNameLabel.frame = CGRect(x: 10, y: albumNameLabel.frame.maxY + 5, width: holder.frame.size.width - 20, height: 30)
         artistNameLabel.text = song.artistName
         holder.addSubview(artistNameLabel)
-
-        // control panel
-        let buttonSize: CGFloat = 70
-        let buttonY = artistNameLabel.frame.maxY + 30
-        let nextButton = UIButton(frame: CGRect(x: holder.frame.size.width - buttonSize - 20, y: buttonY, width: buttonSize, height: buttonSize))
-        let backButton = UIButton(frame: CGRect(x: 20, y: buttonY, width: buttonSize, height: buttonSize))
-        playPauseButton.frame = CGRect(x: (holder.frame.size.width - buttonSize) / 2.0, y: buttonY, width: buttonSize, height: buttonSize)
-
-        playPauseButton.setBackgroundImage(UIImage(systemName: "pause.fill"), for: .normal)
-        backButton.setBackgroundImage(UIImage(systemName: "backward.fill"), for: .normal)
-        nextButton.setBackgroundImage(UIImage(systemName: "forward.fill"), for: .normal)
         
-        playPauseButton.tintColor = .black
-        backButton.tintColor = .black
-        nextButton.tintColor = .black
-        
-        playPauseButton.addTarget(self, action: #selector(didTapPlayPauseButton), for: .touchUpInside)
-        nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
-        backButton.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
-
+        playPauseButton.frame = CGRect(x: (holder.frame.size.width - 100) / 2, y: artistNameLabel.frame.maxY + 10, width: 100, height: 50)
         holder.addSubview(playPauseButton)
-        holder.addSubview(backButton)
-        holder.addSubview(nextButton)
     }
     
     func startTimer() {
@@ -131,51 +129,40 @@ class PlayerViewController: UIViewController {
     }
     
     @objc func updateProgressSlider() {
+        guard let player = player else { return }
         progressSlider.value = Float(player.currentTime)
+        currentTimeLabel.text = formatTime(player.currentTime)
+        durationLabel.text = formatTime(player.duration - player.currentTime)
     }
     
     @objc func progressSliderChanged(_ sender: UISlider) {
-        player.currentTime = TimeInterval(sender.value)
-        if !player.isPlaying {
-            player.play()
-        }
-    }
-    
-    @objc func didTapBackButton() {
-        if position > 0 {
-            position -= 1
-            player?.stop()
-            for subview in holder.subviews {
-                subview.removeFromSuperview()
-            }
-            configure()
-        }
-    }
-    
-    @objc func didTapNextButton() {
-        if position < (songs.count - 1) {
-            position += 1
-            player?.stop()
-            for subview in holder.subviews {
-                subview.removeFromSuperview()
-            }
-            configure()
-        }
-    }
-    
-    @objc func didTapPlayPauseButton() {
-        if player?.isPlaying == true {
-            player?.pause()
-            playPauseButton.setBackgroundImage(UIImage(systemName: "play.fill"), for: .normal)
-        } else {
+        player?.currentTime = TimeInterval(sender.value)
+        if player?.isPlaying == false {
             player?.play()
-            playPauseButton.setBackgroundImage(UIImage(systemName: "pause.fill"), for: .normal)
         }
+    }
+    
+    @objc func playPauseTapped() {
+        guard let player = player else { return }
+        if player.isPlaying {
+            player.pause()
+            playPauseButton.setTitle("Play", for: .normal)
+        } else {
+            player.play()
+            playPauseButton.setTitle("Pause", for: .normal)
+        }
+    }
+    
+    func formatTime(_ time: TimeInterval) -> String {
+        let minutes = Int(time) / 60
+        let seconds = Int(time) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         player?.stop()
+        player = nil
         timer?.invalidate()
     }
 }
